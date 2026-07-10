ab47bf2e8c9e0bb6dd29ee1e40bc4dee5ce93943
quduss <qudshuaib@gmail.com>
2026-07-10 12:44:30 +0100

    Write a function that resolves the filepath of config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,14 @@
+import os from "os";
+import path from "path";
+
+
 type Config = {
   dbUrl: string;
   currentUserName: string;
 };
 
+function getConfigFilePath() {
+  const configFileName = ".gatorconfig.json";
+  const homeDir = os.homedir();
+  return path.join(homeDir, configFileName);
+}
