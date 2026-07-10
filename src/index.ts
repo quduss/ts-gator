bb4172f9a2a7e6acbb6b4ab07c0b591704d18ccc
quduss <qudshuaib@gmail.com>
2026-07-10 15:47:23 +0100

    Update the main function to set the current user to my name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { readConfig, setUser } from "./config";
 function main() {
-  console.log("Hello, world!");
+  setUser("Lane");
+  const cfg = readConfig();
+  console.log(cfg);
 }
 
 main();
