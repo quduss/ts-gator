3c81c3ebae4649910bc9b1d46623561330d78d79
quduss <qudshuaib@gmail.com>
2026-07-11 19:35:56 +0100

    Create a function that registers a new handler function for a command name.

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,3 +1,11 @@
 export type CommandHandler = (cmdName: string, ...args: string[]) => void;
 
 export type CommandsRegistry = Record<string, CommandHandler>;
+
+export function registerCommand(
+  registry: CommandsRegistry,
+  cmdName: string,
+  handler: CommandHandler,
+): void {
+  registry[cmdName] = handler;
+}
