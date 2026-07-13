2326077baab6941cbd8ba7c3de78506b55147eb8
quduss <qudshuaib@gmail.com>
2026-07-13 13:50:24 +0100

    Add the async keyword before all the commands and runCommand

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,4 +1,4 @@
-export type CommandHandler = (cmdName: string, ...args: string[]) => void;
+export type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
 
 export type CommandsRegistry = Record<string, CommandHandler>;
 
@@ -10,11 +10,11 @@ export function registerCommand(
   registry[cmdName] = handler;
 }
 
-export function runCommand(
+export async function runCommand(
   registry: CommandsRegistry,
   cmdName: string,
   ...args: string[]
-): void {
+): Promise<void> {
   const handler = registry[cmdName];
   if (!handler) {
     throw new Error(`Unknown command: ${cmdName}`);
--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,6 +1,6 @@
 import { setUser } from "../config";
 
-export function handlerLogin(cmdName: string, ...args: string[]) {
+export async function handlerLogin(cmdName: string, ...args: string[]) {
   if (args.length !== 1) {
     throw new Error(`usage: ${cmdName} <name>`);
   }
