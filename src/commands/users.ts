52b26ac262e1dd0a20633bc6ea9f537a912fca7a
quduss <qudshuaib@gmail.com>
2026-07-14 13:24:26 +0100

    Create a register handler and register it with the commands

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,4 +1,5 @@
 import { setUser } from "../config";
+import { createUser, getUser } from "../lib/db/queries/users";
 
 export async function handlerLogin(cmdName: string, ...args: string[]) {
   if (args.length !== 1) {
@@ -6,6 +7,25 @@ export async function handlerLogin(cmdName: string, ...args: string[]) {
   }
 
   const userName = args[0];
-  setUser(userName);
+  const existingUser = await getUser(userName);
+  if (!existingUser) {
+    throw new Error(`User ${userName} not found`);
+  }
+  setUser(existingUser.name);
   console.log("User switched successfully!");
+}
+
+export async function handlerRegister(cmdName: string, ...args: string[]) {
+  if (args.length != 1) {
+    throw new Error(`usage: ${cmdName} <name>`);
+  }
+
+  const userName = args[0];
+  const user = await createUser(userName);
+  if (!user) {
+    throw new Error(`User ${userName} not found`);
+  }
+
+  setUser(user.name);
+  console.log("User created successfully!");
 }
