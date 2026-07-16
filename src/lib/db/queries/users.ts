4f8b89412be96be097f06c02f3281be6f236b160
quduss <qudshuaib@gmail.com>
2026-07-16 14:54:28 +0100

    Write a query that deletes all the users in the users table.

--- src/lib/db/queries/users.ts
+++ src/lib/db/queries/users.ts
@@ -12,3 +12,7 @@ export async function getUser(name: string) {
   const result = await db.select().from(users).where(eq(users.name, name));
   return firstOrUndefined(result);
 }
+
+export async function deleteUsers() {
+  await db.delete(users);
+}
