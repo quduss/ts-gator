52702d81d09866480a5a2e54a3d88f534693b554
quduss <qudshuaib@gmail.com>
2026-08-14 21:16:33 +0100

    Write the handler for the following command that prints all the names of feeds the current user is following

--- src/commands/feed-follows.ts
+++ src/commands/feed-follows.ts
@@ -1,5 +1,5 @@
 import { getFeedByURL } from "src/lib/db/queries/feeds";
-import { createFeedFollow } from "../lib/db/queries/feed-follows";
+import { createFeedFollow, getFeedFollowsForUser } from "../lib/db/queries/feed-follows";
 import { readConfig } from "src/config";
 import { getUser } from "src/lib/db/queries/users";
 
@@ -26,6 +26,26 @@ export async function handlerFollow(cmdName: string, ...args: string[]) {
 
   console.log(`Feed follow created:`);
   printFeedFollow(ffRow.userName, ffRow.feedName);
+}
+
+export async function handlerListFeedFollows(_: string) {
+  const config = readConfig();
+  const user = await getUser(config.currentUserName);
+
+  if (!user) {
+    throw new Error(`User ${config.currentUserName} not found`);
+  }
+
+  const feedFollows = await getFeedFollowsForUser(user.id);
+  if (feedFollows.length === 0) {
+    console.log(`No feed follows found for this user.`);
+    return;
+  }
+
+  console.log(`Feed follows for user %s:`, user.id);
+  for (let ff of feedFollows) {
+    console.log(`* %s`, ff.feedname);
+  }
 }
 
 export function printFeedFollow(username: string, feedname: string) {
