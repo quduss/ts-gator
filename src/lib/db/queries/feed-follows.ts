7052106949f7f621dc17bfef7b5016d8746473b1
quduss <qudshuaib@gmail.com>
2026-08-14 12:42:01 +0100

    Add a function that inserts a feed follow record, then returns all the fields from the feed follow as well as the names of the linked user and feed.

---
+++ src/lib/db/queries/feed-follows.ts
@@ -0,0 +1,32 @@
+import { eq, and } from "drizzle-orm";
+import { db } from "..";
+import { feeds, feedFollows, users } from "../schema";
+
+
+export async function createFeedFollow(userId: string, feedId: string) {
+  const [newFeedFollow] = await db
+    .insert(feedFollows)
+    .values({ feedId, userId })
+    .returning();
+
+  const [result] = await db
+    .select({
+      id: feedFollows.id,
+      createdAt: feedFollows.createdAt,
+      updatedAT: feedFollows.updatedAt,
+      userId: feedFollows.userId,
+      feedId: feedFollows.feedId,
+      feedName: feeds.name,
+      userName: users.name,
+    })
+    .from(feedFollows)
+    .innerJoin(feeds, eq(feedFollows.feedId, feeds.id))
+    .innerJoin(users, eq(feedFollows.userId, users.id))
+    .where(
+      and(
+        eq(feedFollows.id, newFeedFollow.id),
+      ),
+    );
+
+  return result;
+}
