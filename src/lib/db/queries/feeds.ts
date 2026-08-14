7d716566ed3aca179497c4ecdd2b3f28f605f24d
quduss <qudshuaib@gmail.com>
2026-08-14 16:07:01 +0100

    Write a function that retrieves a feed by its url from the feeds table

--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -1,7 +1,7 @@
 import { db } from "..";
 import { feeds } from "../schema";
 import { firstOrUndefined } from "./utils";
-
+import { eq } from "drizzle-orm";
 
 
 
@@ -23,4 +23,9 @@ export async function createFeed(
 export async function getFeeds() {
   const result = await db.select().from(feeds);
   return result;
+}
+
+export async function getFeedByURL(url: string) {
+  const result = await db.select().from(feeds).where(eq(feeds.url, url));
+  return firstOrUndefined(result);
 }
