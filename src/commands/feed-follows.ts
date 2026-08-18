08592ecf3c9081a947acadbd08573660920e650e
quduss <qudshuaib@gmail.com>
2026-08-18 16:07:57 +0100

    Create the handler for the unfollow command

--- src/commands/feed-follows.ts
+++ src/commands/feed-follows.ts
@@ -1,5 +1,5 @@
 import { getFeedByURL } from "src/lib/db/queries/feeds";
-import { createFeedFollow, getFeedFollowsForUser } from "../lib/db/queries/feed-follows";
+import { createFeedFollow, deleteFeedFollow, getFeedFollowsForUser } from "../lib/db/queries/feed-follows";
 import { readConfig } from "src/config";
 import { getUser } from "src/lib/db/queries/users";
 import { User } from "src/lib/db/schema";
@@ -39,4 +39,27 @@ export async function handlerListFeedFollows(_: string, user: User) {
 export function printFeedFollow(username: string, feedname: string) {
   console.log(`* User:          ${username}`);
   console.log(`* Feed:          ${feedname}`);
+}
+
+export async function handlerUnfollow(
+  cmdName: string,
+  user: User,
+  ...args: string[]
+) {
+  if (args.length !== 1) {
+    throw new Error(`usage: ${cmdName} <feed_url>`);
+  }
+
+  const feedURL = args[0];
+  let feed = await getFeedByURL(feedURL);
+  if (!feed) {
+    throw new Error(`Feed not found for url: ${feedURL}`);
+  }
+
+  const result = await deleteFeedFollow(feed.id, user.id);
+  if (!result) {
+    throw new Error(`Failed to unfollow feed: ${feedURL}`);
+  }
+
+  console.log(`%s unfollowed successfully!`, feed.name);
 }
