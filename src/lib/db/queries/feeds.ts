fc6b68659dabc179d841e2ec5ce3b0f0028e39c8
quduss <qudshuaib@gmail.com>
2026-08-21 04:50:59 +0100

    Add a function that sets the last_fetched_at and updated_at columns to the current time for a given feed

--- src/lib/db/queries/feeds.ts
+++ src/lib/db/queries/feeds.ts
@@ -29,3 +29,14 @@ export async function getFeedByURL(url: string) {
   const result = await db.select().from(feeds).where(eq(feeds.url, url));
   return firstOrUndefined(result);
 }
+
+export async function markFeedFetched(feedId: string) {
+  const result = await db
+    .update(feeds)
+    .set({
+      lastFetchedAt: new Date(),
+    })
+    .where(eq(feeds.id, feedId))
+    .returning();
+  return firstOrUndefined(result);
+}
