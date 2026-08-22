8923282b07cc2e4432834f22a5903a240a84df45
quduss <qudshuaib@gmail.com>
2026-08-22 12:48:48 +0100

    Write a function that scrapes a Feed and marks it as fetched

--- src/commands/aggregate.ts
+++ src/commands/aggregate.ts
@@ -1,5 +1,6 @@
 import { fetchFeed } from "../lib/rss";
-
+import { markFeedFetched } from "src/lib/db/queries/feeds";
+import { Feed } from "src/lib/db/schema";
 
 export async function handlerAgg(_: string) {
   const feedURL = "https://www.wagslane.dev/index.xml";
@@ -7,4 +8,17 @@ export async function handlerAgg(_: string) {
   const feedData = await fetchFeed(feedURL);
   const feedDataStr = JSON.stringify(feedData, null, 2);
   console.log(feedDataStr);
+}
+
+async function scrapeFeed(feed: Feed) {
+  const feedData = await fetchFeed(feed.url);
+  await markFeedFetched(feed.id);
+
+  for (const item of feedData.channel.item) {
+    console.log(item.title);
+  }
+
+  console.log(
+    `Feed ${feed.name} collected, ${feedData.channel.item.length} posts found`,
+  );
 }
